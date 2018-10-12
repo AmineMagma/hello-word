09b86e4d305a051d3b558ce29d49bebdf407d72d
Amine Magma <magma506@outlook.com>
2018-10-12 22:34:25 +0100

    add client phone number to the pins

--- extension1/ViewController.m
+++ extension1/ViewController.m
@@ -26,10 +26,12 @@
   
     [items setName:@"Paris au france"];
     [items setTimeZone:[NSTimeZone timeZoneWithName:[NSTimeZone localTimeZone].name]];
+    [items setPhoneNumber:@"077 78 48 39"];
      mapItems  = [NSMutableArray arrayWithObjects:items, nil];
     placemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(45.750000, 4.850000)];
     items = [[MKMapItem alloc] initWithPlacemark:placemark];
     [items setName:@"ville lyon"];
+    [items setPhoneNumber:@"053 42 83 95"];
     [mapItems addObject:items];
     NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[NSValue valueWithMKCoordinateSpan:MKCoordinateSpanMake(0.035, 0.035)],MKLaunchOptionsMapSpanKey,[NSNumber numberWithInt:1],MKLaunchOptionsMapTypeKey,MKLaunchOptionsDirectionsModeWalking,MKLaunchOptionsDirectionsModeKey, nil];
     if (![MKMapItem openMapsWithItems:mapItems launchOptions:dic]){
