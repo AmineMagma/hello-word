1c8e3f30e5954fcf5def733430ada32d00693a7c
Amine Magma <magma506@outlook.com>
2018-10-12 15:34:28 +0100

    add hello word label

--- extension1/ViewController.m
+++ extension1/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.width / 2 - 30, 0, 0)];
+    [lab setText:@"Hello Word 😊"];
+    [lab setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleTitle1]];
+    [lab sizeToFit];
+    [[self view] addSubview:lab];
 }
 
 
