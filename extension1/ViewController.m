d69dff94100072df4afdf3e914ceea1ab4a554f5
Amine Magma <magma506@outlook.com>
2018-10-13 17:10:56 +0100

    add notification center and test multi threading with dispatch mechanism

--- extension1/ViewController.m
+++ extension1/ViewController.m
@@ -12,6 +12,9 @@
     MKMapView *map;
     NSMutableArray *mapItems;
     MKCompassButton *compass;
+    NSNotificationName nameNotification;
+    UIButton *bt;
+    
 }
 
 @end
@@ -20,25 +23,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
     MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(48.8534, 2.3488)];
     MKMapItem *items = [[MKMapItem alloc] initWithPlacemark:placemark];
-  
     [items setName:@"Paris au france"];
     [items setTimeZone:[NSTimeZone timeZoneWithName:[NSTimeZone localTimeZone].name]];
     [items setPhoneNumber:@"077 78 48 39"];
-     mapItems  = [NSMutableArray arrayWithObjects:items, nil];
+    mapItems  = [NSMutableArray arrayWithObjects:items, nil];
     placemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(45.750000, 4.850000)];
     items = [[MKMapItem alloc] initWithPlacemark:placemark];
     [items setName:@"ville lyon"];
     [items setPhoneNumber:@"053 42 83 95"];
     [mapItems addObject:items];
-    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[NSValue valueWithMKCoordinateSpan:MKCoordinateSpanMake(0.035, 0.035)],MKLaunchOptionsMapSpanKey,[NSNumber numberWithInt:1],MKLaunchOptionsMapTypeKey,MKLaunchOptionsDirectionsModeWalking,MKLaunchOptionsDirectionsModeKey, nil];
-    if (![MKMapItem openMapsWithItems:mapItems launchOptions:dic]){
-        NSLog(@"error launching map");
-    }
-    
+    NSNotificationCenter *notif = [NSNotificationCenter defaultCenter];
+     nameNotification = @"openMap";
+    [notif addObserver:self selector:@selector(iDemandToOpenMap:) name:nameNotification object:nil];
+    bt = [UIButton buttonWithType:UIButtonTypeSystem];
+    [bt setTitle:@"Show Map" forState:UIControlStateNormal];
+    [bt setFrame:CGRectMake(25, 25, 0, 0)];
+    [bt sizeToFit];
+    [[self view] addSubview:bt];
+    [bt addTarget:self action:@selector(postNotif:) forControlEvents:UIControlEventTouchDown];
 }
-
+-(void)postNotif:(UIButton *)sender{
+    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[NSValue valueWithMKCoordinateSpan:MKCoordinateSpanMake(0.035, 0.035)],MKLaunchOptionsMapSpanKey,[NSNumber numberWithInt:2],MKLaunchOptionsMapTypeKey,MKLaunchOptionsDirectionsModeDriving,MKLaunchOptionsDirectionsModeKey, nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:nameNotification object:dic];
+}
+-(void)iDemandToOpenMap:(NSNotification*)notif{
+    if ([[notif name] isEqualToString:@"openMap"]) {
+        if ([[notif object] isKindOfClass:[NSDictionary class]]) {
+            dispatch_queue_main_t mainQueue = dispatch_get_main_queue();
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW,0);
+            dispatch_sync(queue, ^{
+                NSDictionary *launchOptions = [notif object];
+                [self->bt setTitle:@"Good" forState:UIControlStateNormal];
+                if (![MKMapItem openMapsWithItems:self->mapItems launchOptions:launchOptions]){
+                    NSLog(@"error launching map");
+                    [self->bt setTitle:@"error" forState:UIControlStateNormal];
+                   
+                }
+            });
+            
+        }
+    }
+}
 
 @end
